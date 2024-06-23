242e1149c3a6aa3e9017f8ca859b7aceec0337de
DonkoTonev <donkotonev04@gmail.com>
2024-06-23 18:39:19 +0300

    Just fixed the "Home" page.

--- Task/src/App.jsx
+++ Task/src/App.jsx
@@ -2,8 +2,6 @@
 // Router
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-
 import Home from './pages/home'
 import FirstQuestion from './pages/first-question'
 import ProgressBar from './pages/components/progress-bar'
@@ -12,9 +10,15 @@ import ThirdQuestion from './pages/third-question';
 import FourthQuestion from './pages/fourth-question';
 import FiveQuestion from './pages/five-question';
 
+
+
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import './App.css';
+// import image from './assets/image.jpg';
+
 export default function App() {
   return (
-    
     <BrowserRouter>
       <Routes>
           <Route index element={<Home />} />
--- Task/src/pages/home.jsx
+++ Task/src/pages/home.jsx
@@ -12,26 +12,29 @@ export default function Home() {
     // };
 
     return (
-        <div className="container">
-            <div className="image-container">
-                <img src={image} className="top-image" alt="Descriptive Alt Text" />
-                <div className="centered-div">
-                    <div className="title-container">
-                        Build a self care routine suitable for you
-                        <div className="text-container">
-                            Take our test to get a personalized self care routine based on your needs.
-                        </div>
-                    </div>
-                    {/* Using Link for navigation */}
-                    <Link to="/first" className='start-button'>
-                        <span className='button-text'>Start the quiz</span>
-                    </Link>
-                    {/* Alternatively, you can use a button with handleClick function */}
-                    {/* <button className='start-button' onClick={handleClick}>
-                        <span className='button-text'>Start the quiz</span>
-                    </button> */}
-                </div>
-            </div>
-        </div>
+        // <BrowserRouter>
+          <div className="home-container">
+              <div className="image-container">
+                  <img src={image} className="top-image" alt="Descriptive Alt Text" />
+                  <div className="centered-div">
+                      <div className="title-container">
+                          Build a self care routine suitable for you
+                          <div className="text-container">
+                              Take our test to get a personalized self care routine based on your needs.
+                          </div>
+                      </div>
+                      {/* Using Link for navigation */}
+                      <button className='start-button'><Link to="/first">
+                          <span className='button-text'>Start the quiz</span>
+                      </Link></button>
+                      
+                      {/* Alternatively, you can use a button with handleClick function */}
+                      {/* <button className='start-button'>
+                          <span className='button-text'>Start the quiz</span>
+                      </button> */}
+                  </div>
+              </div>
+          </div>
+        // </BrowserRouter>
     );
 }
