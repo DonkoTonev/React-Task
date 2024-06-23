ddfa3ed6e9034ddf58d673b27329e37a4d21a327
DonkoTonev <donkotonev04@gmail.com>
2024-06-23 20:59:22 +0300

    Quick update

--- Task/src/App.jsx
+++ Task/src/App.jsx
@@ -10,25 +10,84 @@ import ThirdQuestion from './pages/third-question';
 import FourthQuestion from './pages/fourth-question';
 import FiveQuestion from './pages/five-question';
 
-
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid'
+// import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import './App.css';
-// import image from './assets/image.jpg';
+import { Link } from 'react-router-dom';
+import './App.css';
+import image from './assets/image.jpg';
+
+
+
+
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  fontawesome.library.add(faArrowRight);
+
   return (
     <BrowserRouter>
-      <Routes>
-          <Route index element={<Home />} />
-          {/* <Route index element={<ProgressBar />} /> */}
-          <Route path='/first' element={<FirstQuestion />} />
-          <Route path='/second' element={<SecondQuestion />} />
-          <Route path='/third' element={<ThirdQuestion />} />
-          <Route path='/fourth' element={<FourthQuestion />} />
-          <Route path='/fifth' element={<FiveQuestion />} />
-      </Routes>
+      {/* <> */}
+      <div className="container">
+        <div className='question'>
+          What's your hair type or texture?
+        </div>
+        <div className='answers-container'>
+          <button className='answer'><span className='answer-text'>a. Straight</span></button>
+          <button className='answer'><span className='answer-text'>b. Curly</span></button>
+          <button className='answer'><span className='answer-text'>c. Wavy</span></button>
+          <button className='answer'><span className='answer-text'>d. Fine</span></button>
+        </div>
+
+        <div className='next-back'>
+          <a href='/' className='back-link'>Back</a>
+          <button className='button-next'>
+            <Link to="/second">
+              <span className='button-text'>Next question</span>
+              
+            </Link>
+            <FontAwesomeIcon className='arrow-icon' icon={faArrowRight} />
+          </button>
+          {/* <button to="/first" className='button'><span className='button-text'>Next question</span></button> */}
+        </div>
+      </div>
+      {/* <ProgressBar />
+        </> */}
     </BrowserRouter>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//           <Route index element={<Home />} />
+//           {/* <Route index element={<ProgressBar />} /> */}
+//           <Route path='/first' element={<FirstQuestion />} />
+//           <Route path='/second' element={<SecondQuestion />} />
+//           <Route path='/third' element={<ThirdQuestion />} />
+//           <Route path='/fourth' element={<FourthQuestion />} />
+//           <Route path='/fifth' element={<FiveQuestion />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
--- Task/src/pages/first-question.jsx
+++ Task/src/pages/first-question.jsx
@@ -9,26 +9,30 @@ export default function FirstQuestion() {
 
     return (
         <>
-        <div className="container">
-            <div className='question'>
-                What's your hair type or texture?
+            <div className="container">
+                <div className='question'>
+                    What's your hair type or texture?
+                </div>
+
+                <div className='answers-container'>
+                    <button className='answer'><span className='answer-text'>a. Straight</span></button>
+                    <button className='answer'><span className='answer-text'>b. Curly</span></button>
+                    <button className='answer'><span className='answer-text'>c. Wavy</span></button>
+                    <button className='answer'><span className='answer-text'>d. Fine</span></button>
+                </div>
+
+                <div className='next-back'>
+                    <a href='/' className='back-link'>Back</a>
+                    <button className='button'>
+                        <Link to="/second">
+                            <span className='button-text'>Next question</span>
+                            <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                        </Link></button>
+                    {/* <button to="/first" className='button'><span className='button-text'>Next question</span></button> */}
+                </div>
             </div>
 
-            <div className='answers-container'>
-                <button className='answer'><span className='answer-text'>a. Straight</span></button>
-                <button className='answer'><span className='answer-text'>b. Curly</span></button>
-                <button className='answer'><span className='answer-text'>c. Wavy</span></button>
-                <button className='answer'><span className='answer-text'>d. Fine</span></button>
-            </div>
-
-            <div className='next-back'>
-                <a href='/' className='back-link'>Back</a>
-                <Link to="/second" className='button'><span className='button-text'>Next question</span></Link>
-                {/* <button to="/first" className='button'><span className='button-text'>Next question</span></button> */}
-            </div>
-        </div>
-
-        <ProgressBar />
+            <ProgressBar />
         </>
     );
 }
